5d686194e82aaa97d3fe1aa4aefbfd4ca94f2a74
Юлия <118264321+Morozova91@users.noreply.github.com>
2024-05-20 18:59:42 +0300

    homework4.py

--- homework4.py
+++ homework4.py
@@ -1,14 +1,14 @@
 # Списки и кортежи
 # 1.
 immutable_var = (1,4,True, [125, 404, 8], 'tasks')
-print(immutable_var)
+print(f'Immutable_tupple: ',immutable_var)
 # 2. Так как кортеж - неизменяемый тип данных, будет возникать
 # ошибка при попытке изменить данные внутри кортежа (кортеж не поддерживает обращение по элементам)
 immutable_var[0] = 8
-print(immutable_var)
+print(f'Modified_immutable_tupple: ', immutable_var)
 # 3.
 mutable_list = [5, 150, 9 ,652, 'name']
-print(mutable_list)
+print(f'Mutable list: ', mutable_list)
 mutable_list.append('last_name')
-print(mutable_list)
+print(f'Modified_mutable_list: ', mutable_list)
 
